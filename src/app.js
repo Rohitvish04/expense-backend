f8ee170558bfee8460f5c6817638fffecc4c5d2a
Rohit <rohitvvishwakarma400@gmail.com>
2025-12-28 11:48:12 +0530

    change cors origin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ const authRoutes = require("./routes/auth.routes");
 const expenseRoutes = require("./routes/expense.routes");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or your vercel URL
+    credentials: true,
+  })
+);
 app.use(express.json());    
 app.use(express.urlencoded({ extended: true })); 
 
