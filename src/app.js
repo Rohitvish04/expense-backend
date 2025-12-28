76bb85294bd86469567398d9fa01b1ef7e28af52
Rohit <rohitvvishwakarma400@gmail.com>
2025-12-28 12:43:30 +0530

    change origin

--- src/app.js
+++ src/app.js
@@ -5,15 +5,17 @@ const authRoutes = require("./routes/auth.routes");
 const expenseRoutes = require("./routes/expense.routes");
 
 const app = express();
-app.use(
-  cors({
-    origin: [
-    //   "http://localhost:5173",
-      "https://expense-frontend-71fk9qhbz.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: [
+    // "http://localhost:5173", // local frontend
+    "https://expense-frontend-cmcdvj4af.vercel.app" // deployed frontend
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(express.json());    
 app.use(express.urlencoded({ extended: true })); 
 
